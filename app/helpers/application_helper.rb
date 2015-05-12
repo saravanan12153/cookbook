0be0317619e454c2898820da87195e35026bd4d1
rdurel423 <rdurel423@gmail.com>
2015-05-12 16:16:00 -0500

    Added chef information to recipe list. Added method for getting a users gravatar image.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def gravatar_for(chef, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest chef.email.downcase
+    size = options[:size]
+    url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+
+    image_tag(url, alt: chef.name, class: 'gravatar')
+  end
+
 end
