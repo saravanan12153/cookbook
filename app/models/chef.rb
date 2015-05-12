1de4b13d02d680e9e07fa287324c63bf21742754
rdurel423 <rdurel423@gmail.com>
2015-05-12 14:10:21 -0500

    Added validations to the Chef model.

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,2 +1,7 @@
 class Chef < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 40 }
+  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
