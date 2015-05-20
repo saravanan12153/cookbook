5d9df26540fc8e6fc01c7fb06c0ea12dcbdd1ec2
rdurel423 <rdurel423@gmail.com>
2015-05-20 14:03:11 -0500

    Added the registration page.

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,4 +1,37 @@
 class ChefsController < ApplicationController
   def new
+    @chef = Chef.new
+  end
+
+  def create
+    @chef = Chef.new chef_params
+
+    if @chef.save
+      flash[:success] = 'Your account has been successfully created.'
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def chef_params
+    params.require(:chef).permit :name, :email, :password
   end
 end
